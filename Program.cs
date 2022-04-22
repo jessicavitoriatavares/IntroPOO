7022229264e012fd8f157b09bbea48e803b9cf22
jessicavitoriatavares <102222297+jessicavitoriatavares@users.noreply.github.com>
2022-04-22 12:45:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Quadrado q1 = new Quadrado(10.0);
+Console.WriteLine(q1.Lado);
+q1.Lado = 8.0;
+Console.WriteLine(q1.Area);
+Console.WriteLine(q1.Perimetro);
+
+Console.WriteLine("Separando");
+
+Retangulo r1 = new Retangulo(10, 10);
+Console.WriteLine(r1.Base);
+Console.WriteLine(r1.Altura);
+Console.WriteLine(r1.Perimetro);
+Console.WriteLine(r1.Area);
